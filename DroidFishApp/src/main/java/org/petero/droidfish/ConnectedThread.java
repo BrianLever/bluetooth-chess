4f8aeba83923f7d966f357dc45549234c5c3ff35
CemenLangemec <goldmastermind@outlook.com>
2022-03-22 03:29:28 +0800

    bluetooh function adding

---
+++ DroidFishApp/src/main/java/org/petero/droidfish/ConnectedThread.java
@@ -0,0 +1,59 @@
+package org.petero.droidfish;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.SystemClock;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ConnectedThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+    private final Handler mHandler;
+    public ConnectedThread(BluetoothSocket socket, Handler handler) {
+        mmSocket = socket;
+        mHandler = handler;
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+        try {
+            tmpIn = socket.getInputStream();
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) { }
+        mmInStream = tmpIn;
+        mmOutStream = tmpOut;
+    }
+    @Override
+    public void run() {
+        byte[] buffer;
+        int bytes;
+        while (true) {
+            try {
+                bytes = mmInStream.available();
+                if(bytes != 0) {
+                    buffer = new byte[bytes];
+                    bytes = mmInStream.available();
+                    bytes = mmInStream.read(buffer, 0, bytes);
+                    mHandler.obtainMessage(DroidFish.MESSAGE_READ, bytes, -1, buffer)
+                        .sendToTarget();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+    public void write(String input) {
+        byte[] bytes = input.getBytes();
+        try {
+            mmOutStream.write(bytes);
+        } catch (IOException e) { }
+    }
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
+    }
+}
